6d71af85afb1ecedf8cf15a85ee0a72ee7192c0a
masoomahasnain <masooma.punjwani@student.moringaschool.com>
2022-10-27 16:10:50 +0300

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
+class Book
+    attr_accessor :title, :author, :page_count, :turn_page
 
+    def initialize(title)
+        @title = title
+    end
+
+    def turn_page
+        @turn_page
+        puts "It's an amazing book!"
+    end
+
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
 # Make your shoe class here!
+class shoe
+    attr_accessor :brand, :color, :size, :color :material
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        @condition = "new"
+        puts "My shoe is perfect"
+    end
+
+end
